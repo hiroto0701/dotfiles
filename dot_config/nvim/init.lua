226d2f554ce8216ccef649d522ec5f1ef13879a5
hiroto inagaki <hiroto.soccer.0701@gmail.com>
2025-12-24 21:50:54 +0900

    feat(nvim): lualineに設定を追加して複数ファイルをタブで開けるようにした

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -7,6 +7,12 @@ vim.cmd("set shiftwidth=2")
 -- リーダーキーをスペースに設定
 vim.g.mapleader= " "
 
+-- バッファ（タブ）操作のキーマップ
+vim.keymap.set("n", "<Tab>", ":bnext<CR>", { silent = true, desc = "次のバッファへ" })
+vim.keymap.set("n", "<S-Tab>", ":bprev<CR>", { silent = true, desc = "前のバッファへ" })
+vim.keymap.set("n", "<leader>x", ":bd<CR>", { silent = true, desc = "バッファを閉じる" })
+vim.keymap.set("n", "<leader>X", ":bd!<CR>", { silent = true, desc = "バッファを強制的に閉じる" })
+
 -- Lazy.nvimのブートストラップ
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
