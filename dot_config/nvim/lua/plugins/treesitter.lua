20b324eb6003286c4be234fa084b8fa03cd0abb9
hiroto inagaki <hiroto.soccer.0701@gmail.com>
2025-12-26 00:41:37 +0900

    add(nvim): auto-close-tag導入

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,31 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   build = ":TSUpdate",
-  opts = {
-    ensure_installed = { "lua", "vim", "vimdoc" },
-    auto_install = true,
-    highlight = {
-      enable = true,
-    },
-    indent = {
-      enable = true,
-    },
-  },
+  config = function()
+    require("nvim-treesitter").setup({
+      install_dir = vim.fn.stdpath("data") .. "/site",
+    })
+
+    require("nvim-treesitter").install({
+      "lua",
+      "vim",
+      "vimdoc",
+      "html",
+      "css",
+      "javascript",
+      "typescript",
+      "tsx",
+      "json",
+      "markdown",
+      "markdown_inline",
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "*",
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
+    })
+  end,
 }
