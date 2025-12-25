6a26c206749e4886deb687ba08870d7a772807cb
bnx-inagaki <Hiroto_Inagaki@bandainamco-nexus.co.jp>
2025-12-25 17:24:56 +0900

    feat: barbarの設定変更

---
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,27 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    signs = {
+      add = { text = "│" },
+      change = { text = "│" },
+      delete = { text = "󰍵" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+      untracked = { text = "┆" },
+    },
+    signs_staged = {
+      add = { text = "│" },
+      change = { text = "│" },
+      delete = { text = "󰍵" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+      untracked = { text = "┆" },
+    },
+    current_line_blame = false, -- trueにすると現在行に誰がいつ変更したか表示
+    current_line_blame_opts = {
+      delay = 500,
+    },
+  },
+}
+
