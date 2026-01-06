e148834f11ce6565e2b0d4530f6e50acabbc350d
hiroto inagaki <hiroto.soccer.0701@gmail.com>
2026-01-06 16:01:46 +0900

    chore(nvim): change dashboard icon

--- dot_config/nvim/lua/plugins/snacks/dashboard.lua
+++ dot_config/nvim/lua/plugins/snacks/dashboard.lua
@@ -10,19 +10,19 @@ return {
 ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
 ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
     keys = {
-      { icon = "󰈔", key = "e", desc = "New file", action = ":ene | startinsert" },
-      { icon = "󰒲", key = "z", desc = "Lazy", action = ":Lazy" },
-      { icon = "", key = "d", desc = "Dotfiles", action = ":e ~/.config/nvim/init.lua" },
-      { icon = "󰈫", key = "f", desc = "Files", action = ":Telescope find_files" },
-      { icon = "󰒮", key = "s", desc = "Restore Session", action = ":lua require('persistence').load()" },
-      { icon = "󰅚", key = "q", desc = "Quit", action = ":qa" },
+      { icon = " ", key = "e", desc = "New file", action = ":ene | startinsert" },
+      { icon = "󰒲 ", key = "z", desc = "Lazy", action = ":Lazy" },
+      { icon = " ", key = "d", desc = "Dotfiles", action = ":e ~/.local/share/chezmoi" },
+      { icon = "󰱼 ", key = "f", desc = "Find Files", action = ":Telescope find_files" },
+      { icon = "󰑓 ", key = "s", desc = "Restore Session", action = ":lua require('persistence').load()" },
+      { icon = "󰅚 ", key = "q", desc = "Quit", action = ":qa" },
     },
   },
   sections = {
     { section = "header" },
-    { title = "Recent Files", icon = "󰈔 ", section = "recent_files", indent = 2,  padding = 1 },
-    { title = "Projects", icon = "", section = "projects", indent = 2, padding = 1 },
-    { title = "Keymaps", icon = "", section = "keys", indent = 2, padding = 1 },
+    { title = "Recent Files", icon = " ", section = "recent_files", indent = 2, padding = 1 },
+    { title = "Projects", icon = " ", section = "projects", indent = 2, padding = 1 },
+    { title = "Keymaps", icon = " ", section = "keys", indent = 2, padding = 1 },
     { section = "startup" },
   },
 }
